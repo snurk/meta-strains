2c2f083fba00592020c43d9e0b3238aa05ed8b0b
Margarita Akseshina <novmargo@gmail.com>
2018-05-04 15:35:04 +0300

    add corrected reads to pipeline

--- scripts/scripts_for_desman/edges_lengths.py
+++ scripts/scripts_for_desman/edges_lengths.py
@@ -1,14 +1,19 @@
 import sys
-from Bio import SeqIO
- 
 
 def cp_long_seq(input_file, output_file):
     input_handle = open(input_file, 'rU')
     output_handle = open(output_file, "w")
     
-    for record in SeqIO.parse(input_handle, "fasta"):
-        if int(record.id) % 2 == 0:
-            output_handle.write(record.id+'\t'+str(len(record.seq))+'\n')                        
+
+    for line in input_handle:
+        line = line.strip().split()
+        line_type = line[0]
+        if line_type != 'S':
+            break
+        e_id = line[1]
+        e_seq = line[2]
+
+        output_handle.write(e_id+'\t'+str(len(e_seq))+'\n')                        
     
     input_handle.close()
     output_handle.close()
